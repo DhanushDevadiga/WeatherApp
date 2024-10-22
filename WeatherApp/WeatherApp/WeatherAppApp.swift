cc7bfdeb8d76a5fff3d04de2435e977e8dacba9c
Dhanush Devadiga <>
2024-10-22 22:30:47 +0530

    Add Project file

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dhanush Devadiga on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
